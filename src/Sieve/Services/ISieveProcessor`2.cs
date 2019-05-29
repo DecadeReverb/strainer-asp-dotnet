cb4e0cbd6fe964c873375779d73321c90f02af9d
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-05-29 14:15:18 +0200

    Split ISieveProcessor interface

---
+++ src/Sieve/Services/ISieveProcessor`2.cs
@@ -0,0 +1,12 @@
+﻿using Sieve.Models;
+
+namespace Sieve.Services
+{
+    public interface ISieveProcessor<TFilterTerm, TSortTerm>
+        : ISieveProcessor<SieveModel<TFilterTerm, TSortTerm>, TFilterTerm, TSortTerm>
+        where TFilterTerm : IFilterTerm, new()
+        where TSortTerm : ISortTerm, new()
+    {
+
+    }
+}
