9a7beedc3521579b74ec273a99ea2349fea1f0b4
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-05-31 20:02:49 +0200

    Remove generic version of sieve processor

--- src/Sieve/Services/ISieveProcessor.cs
+++ src/Sieve/Services/ISieveProcessor.cs
@@ -1,9 +1,16 @@
-﻿using Sieve.Models;
+﻿using System.Linq;
+using Sieve.Models;
 
 namespace Sieve.Services
 {
-    public interface ISieveProcessor : ISieveProcessor<SieveModel>
+    public interface ISieveProcessor
     {
-
+        IQueryable<TEntity> Apply<TEntity>(
+            ISieveModel model,
+            IQueryable<TEntity> source,
+            object[] dataForCustomMethods = null,
+            bool applyFiltering = true,
+            bool applySorting = true,
+            bool applyPagination = true);
     }
 }
