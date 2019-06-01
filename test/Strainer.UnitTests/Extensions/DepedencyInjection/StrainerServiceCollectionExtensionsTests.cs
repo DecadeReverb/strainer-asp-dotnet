8d94278ad0b83a162b04b024a74c9886b00ca389
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-06-01 19:50:48 +0200

    Add more unit tests for DI extensions

--- test/Strainer.UnitTests/Extensions/DepedencyInjection/StrainerServiceCollectionExtensionsTests.cs
+++ test/Strainer.UnitTests/Extensions/DepedencyInjection/StrainerServiceCollectionExtensionsTests.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using Fluorite.Extensions.DependencyInjection;
 using Fluorite.Strainer.Services;
+using Fluorite.Strainer.UnitTests.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
@@ -32,6 +33,56 @@ namespace Fluorite.Strainer.UnitTests.Extensions.DepedencyInjection
                 .Should()
                 .NotBeNull("Because extension method should add " +
                         "Strainer to the service collection.");
+        }
+
+        [Fact]
+        public void ExtensionMethod_ShouldAddCustomFilterMethods()
+        {
+            // Arrange
+            IConfiguration configuration = new ConfigurationRoot(new List<IConfigurationProvider>());
+            var services = new ServiceCollection().AddSingleton(configuration);
+
+            // Act
+            var serviceProvider = services.BuildServiceProvider();
+            var preExtensionStrainerCustomFilterMethods = serviceProvider.GetService<IStrainerCustomFilterMethods>();
+            services.AddStrainer<StrainerProcessor>()
+                .AddCustomFilterMethods<StrainerCustomFilterMethods>();
+            serviceProvider = services.BuildServiceProvider();
+            var postExtensionStrainerCustomFilterMethods = serviceProvider.GetService<IStrainerCustomFilterMethods>();
+
+            // Assert
+            preExtensionStrainerCustomFilterMethods
+                .Should()
+                .BeNull("Because Strainer has not been registered yet.");
+            postExtensionStrainerCustomFilterMethods
+                .Should()
+                .NotBeNull("Because extension method should add Stariner " +
+                        "custom filter methods to the service collection.");
+        }
+
+        [Fact]
+        public void ExtensionMethod_ShouldAddCustomSortMethods()
+        {
+            // Arrange
+            IConfiguration configuration = new ConfigurationRoot(new List<IConfigurationProvider>());
+            var services = new ServiceCollection().AddSingleton(configuration);
+
+            // Act
+            var serviceProvider = services.BuildServiceProvider();
+            var preExtensionStrainerCustomSortMethods = serviceProvider.GetService<IStrainerCustomSortMethods>();
+            services.AddStrainer<StrainerProcessor>()
+                .AddCustomSortMethods<StrainerCustomSortMethods>();
+            serviceProvider = services.BuildServiceProvider();
+            var postExtensionStrainerCustomSortMethods = serviceProvider.GetService<IStrainerCustomSortMethods>();
+
+            // Assert
+            preExtensionStrainerCustomSortMethods
+                .Should()
+                .BeNull("Because Strainer has not been registered yet.");
+            postExtensionStrainerCustomSortMethods
+                .Should()
+                .NotBeNull("Because extension method should add Stariner " +
+                        "custom sort methods to the service collection.");
         }
     }
 }
