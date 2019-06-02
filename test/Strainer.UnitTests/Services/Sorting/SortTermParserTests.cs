bbe01982724989f611c208ce07e7dc253aee992d
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-06-02 11:57:15 +0200

    Add basic tests for sort term parser

---
+++ test/Strainer.UnitTests/Services/Sorting/SortTermParserTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using Fluorite.Strainer.Services.Sorting;
+using Xunit;
+
+namespace Fluorite.Strainer.UnitTests.Services.Sorting
+{
+    public class SortTermParserTests
+    {
+        [Fact]
+        public void Parser_ReturnsSortTerm_WhenInputIsNull()
+        {
+            // Arrange
+            string input = null;
+            ISortTermParser parser = new SortTermParser();
+
+            // Act
+            var sortTermList = parser.GetParsedTerms(input);
+
+            // Assert
+            sortTermList
+                .Should()
+                .BeEmpty();
+        }
+
+        [Fact]
+        public void Parser_ReturnsSortTerm_WhenInputIsEmpty()
+        {
+            // Arrange
+            var input = string.Empty;
+            ISortTermParser parser = new SortTermParser();
+
+            // Act
+            var sortTermList = parser.GetParsedTerms(input);
+
+            // Assert
+            sortTermList
+                .Should()
+                .BeEmpty();
+        }
+
+        [Fact]
+        public void Parser_ReturnsSortTerm_WhenInputIsOnlyWhitespace()
+        {
+            // Arrange
+            var input = string.Empty;
+            ISortTermParser parser = new SortTermParser();
+
+            // Act
+            var sortTermList = parser.GetParsedTerms(input);
+
+            // Assert
+            sortTermList
+                .Should()
+                .BeEmpty();
+        }
+    }
+}
