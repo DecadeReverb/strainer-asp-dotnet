06215909484e5309ab9b3769b74854a7a349a06c
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-06-02 11:59:23 +0200

    Add basic tests for filter term parser

---
+++ test/Strainer.UnitTests/Services/Filtering/FilterTermParserTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Fluorite.Strainer.Services.Filtering;
+using Xunit;
+
+namespace Fluorite.Strainer.UnitTests.Services.Filtering
+{
+    public class FilterTermParserTests
+    {
+        [Fact]
+        public void Parser_ReturnsFilterTerm_WhenInputIsNull()
+        {
+            // Arrange
+            string input = null;
+            IFilterOperatorProvider operatorProvider = new FilterOperatorProvider();
+            IFilterOperatorParser operatorParser = new FilterOperatorParser(operatorProvider);
+            IFilterTermParser parser = new FilterTermParser(operatorParser);
+
+            // Act
+            var FilterTermList = parser.GetParsedTerms(input);
+
+            // Assert
+            FilterTermList
+                .Should()
+                .BeEmpty();
+        }
+
+        [Fact]
+        public void Parser_ReturnsFilterTerm_WhenInputIsEmpty()
+        {
+            // Arrange
+            var input = string.Empty;
+            IFilterOperatorProvider operatorProvider = new FilterOperatorProvider();
+            IFilterOperatorParser operatorParser = new FilterOperatorParser(operatorProvider);
+            IFilterTermParser parser = new FilterTermParser(operatorParser);
+
+            // Act
+            var FilterTermList = parser.GetParsedTerms(input);
+
+            // Assert
+            FilterTermList
+                .Should()
+                .BeEmpty();
+        }
+
+        [Fact]
+        public void Parser_ReturnsFilterTerm_WhenInputIsOnlyWhitespace()
+        {
+            // Arrange
+            var input = string.Empty;
+            IFilterOperatorProvider operatorProvider = new FilterOperatorProvider();
+            IFilterOperatorParser operatorParser = new FilterOperatorParser(operatorProvider);
+            IFilterTermParser parser = new FilterTermParser(operatorParser);
+
+            // Act
+            var FilterTermList = parser.GetParsedTerms(input);
+
+            // Assert
+            FilterTermList
+                .Should()
+                .BeEmpty();
+        }
+    }
+}
