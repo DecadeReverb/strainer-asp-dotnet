144f7f85aeb711ed7504f3e2ae2294af31065151
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-06-06 17:58:19 +0200

    Add unit tests for filter operator parser

---
+++ test/Strainer.UnitTests/Services/Filtering/FilterOperatorParserTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Fluorite.Strainer.Services.Filtering;
+using Xunit;
+
+namespace Fluorite.Strainer.UnitTests.Services.Filtering
+{
+    public class FilterOperatorParserTests
+    {
+        [Fact]
+        public void Parser_ReturnsDefaultFilterOperator_WhenInputIsNull()
+        {
+            // Arrange
+            string input = null;
+            IFilterOperatorProvider provider = new FilterOperatorProvider();
+            IFilterOperatorParser parser = new FilterOperatorParser(provider);
+
+            // Act
+            var defaultFilterOperator = provider.GetDefaultOperator();
+            var filterOperator = parser.GetParsedOperator(input);
+
+            // Assert
+            filterOperator
+                .Should()
+                .Be(defaultFilterOperator);
+        }
+
+        [Fact]
+        public void Parser_ReturnsDefaultFilterOperator_WhenInputIsEmpty()
+        {
+            // Arrange
+            var input = string.Empty;
+            IFilterOperatorProvider provider = new FilterOperatorProvider();
+            IFilterOperatorParser parser = new FilterOperatorParser(provider);
+
+            // Act
+            var defaultFilterOperator = provider.GetDefaultOperator();
+            var filterOperator = parser.GetParsedOperator(input);
+
+            // Assert
+            filterOperator
+                .Should()
+                .Be(defaultFilterOperator);
+        }
+
+        [Fact]
+        public void Parser_ReturnsDefaultFilterOperator_WhenInputIsWhitespace()
+        {
+            // Arrange
+            var input = " ";
+            IFilterOperatorProvider provider = new FilterOperatorProvider();
+            IFilterOperatorParser parser = new FilterOperatorParser(provider);
+
+            // Act
+            var defaultFilterOperator = provider.GetDefaultOperator();
+            var filterOperator = parser.GetParsedOperator(input);
+
+            // Assert
+            filterOperator
+                .Should()
+                .Be(defaultFilterOperator);
+        }
+    }
+}
