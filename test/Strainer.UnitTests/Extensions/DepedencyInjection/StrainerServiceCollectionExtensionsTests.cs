be0ef2f5759bb6c70ecf4dfc0183644886bd7fc8
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-06-06 17:48:58 +0200

    Add unit test for adding custom filter term parser

--- test/Strainer.UnitTests/Extensions/DepedencyInjection/StrainerServiceCollectionExtensionsTests.cs
+++ test/Strainer.UnitTests/Extensions/DepedencyInjection/StrainerServiceCollectionExtensionsTests.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using Fluorite.Extensions.DependencyInjection;
 using Fluorite.Strainer.Models;
 using Fluorite.Strainer.Services;
+using Fluorite.Strainer.Services.Filtering;
 using Fluorite.Strainer.UnitTests.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,7 +16,7 @@ namespace Fluorite.Strainer.UnitTests.Extensions.DepedencyInjection
     public class StrainerServiceCollectionExtensionsTests
     {
         [Fact]
-        public void ExtensionMethod_ShouldAddStrainer()
+        public void ExtensionMethod_AddsStrainer()
         {
             // Arrange
             IConfiguration configuration = new ConfigurationRoot(new List<IConfigurationProvider>());
@@ -38,7 +40,7 @@ namespace Fluorite.Strainer.UnitTests.Extensions.DepedencyInjection
         }
 
         [Fact]
-        public void ExtensionMethod_ShouldAddStrainerWhileConfiguringOptions()
+        public void ExtensionMethod_AddsStrainerWithCustomOptions()
         {
             // Arrange
             var defaultPageSize = 20;
@@ -47,7 +49,6 @@ namespace Fluorite.Strainer.UnitTests.Extensions.DepedencyInjection
 
             // Act
             var serviceProvider = services.BuildServiceProvider();
-            var preExtensionStrainer = serviceProvider.GetService<IStrainerProcessor>();
             services.AddStrainer<StrainerProcessor>(options =>
             {
                 options.DefaultPageSize = defaultPageSize;
@@ -57,9 +58,6 @@ namespace Fluorite.Strainer.UnitTests.Extensions.DepedencyInjection
             var postExtensionStrainerOptions = serviceProvider.GetService<IOptions<StrainerOptions>>()?.Value;
 
             // Assert
-            preExtensionStrainer
-                .Should()
-                .BeNull("Because Strainer has not been registered yet.");
             postExtensionStrainer
                 .Should()
                 .NotBeNull("Because extension method should add " +
@@ -71,7 +69,33 @@ namespace Fluorite.Strainer.UnitTests.Extensions.DepedencyInjection
         }
 
         [Fact]
-        public void ExtensionMethod_ShouldAddCustomFilterMethods()
+        public void ExtensionMethod_AddsStrainerWithCustomService_FilterTermParser()
+        {
+            // Arrange
+            IConfiguration configuration = new ConfigurationRoot(new List<IConfigurationProvider>());
+            var services = new ServiceCollection().AddSingleton(configuration);
+
+            // Act
+            services.AddScoped<IFilterTermParser, _TestFilterTermParser>();
+            var serviceProvider = services.BuildServiceProvider();
+            services.AddScoped<IFilterTermParser, _TestFilterTermParser>();
+            services.AddStrainer<StrainerProcessor>();
+            serviceProvider = services.BuildServiceProvider();
+            var postExtensionStrainer = serviceProvider.GetService<IStrainerProcessor>();
+            var postExtensionFilterTermParser = serviceProvider.GetService<IFilterTermParser>();
+
+            // Assert
+            postExtensionStrainer
+                .Should()
+                .NotBeNull("Because extension method should add " +
+                        "Strainer to the service collection.");
+            postExtensionFilterTermParser
+                .Should()
+                .BeAssignableTo<_TestFilterTermParser>();
+        }
+
+        [Fact]
+        public void ExtensionMethod_AddsCustomFilterMethods()
         {
             // Arrange
             IConfiguration configuration = new ConfigurationRoot(new List<IConfigurationProvider>());
@@ -96,7 +120,7 @@ namespace Fluorite.Strainer.UnitTests.Extensions.DepedencyInjection
         }
 
         [Fact]
-        public void ExtensionMethod_ShouldAddCustomSortMethods()
+        public void ExtensionMethod_AddsCustomSortMethods()
         {
             // Arrange
             IConfiguration configuration = new ConfigurationRoot(new List<IConfigurationProvider>());
@@ -118,6 +142,14 @@ namespace Fluorite.Strainer.UnitTests.Extensions.DepedencyInjection
                 .Should()
                 .NotBeNull("Because extension method should add Stariner " +
                         "custom sort methods to the service collection.");
+        }
+    }
+
+    internal class _TestFilterTermParser : IFilterTermParser
+    {
+        public IList<IFilterTerm> GetParsedTerms(string input)
+        {
+            throw new NotImplementedException();
         }
     }
 }
