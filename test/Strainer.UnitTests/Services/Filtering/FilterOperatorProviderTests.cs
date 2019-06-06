fdfcd7d3430972deb629eef03956537a621371d1
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-06-06 18:15:52 +0200

    Add unit tests for filter operator provider

---
+++ test/Strainer.UnitTests/Services/Filtering/FilterOperatorProviderTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Fluorite.Strainer.Services.Filtering;
+using Xunit;
+
+namespace Fluorite.Strainer.UnitTests.Services.Filtering
+{
+    public class FilterOperatorProviderTests
+    {
+        [Fact]
+        public void Provider_ReturnsDefaultFilterOperator()
+        {
+            // Arrange
+            IFilterOperatorProvider provider = new FilterOperatorProvider();
+
+            // Act
+            var defaultFilterOperator = provider.GetDefaultOperator();
+
+            // Assert
+            defaultFilterOperator
+                .Should()
+                .NotBeNull();
+            defaultFilterOperator
+                .IsDefault
+                .Should()
+                .BeTrue();
+        }
+
+        [Fact]
+        public void Provider_ReturnsDefaultFilterOperator_WhenNoMatchingOperatorIsFound()
+        {
+            // Arrange
+            var filterOperator = string.Empty;
+            IFilterOperatorProvider provider = new FilterOperatorProvider();
+
+            // Act
+            var defaultFilterOperator = provider.GetFirstOrDefault(filterOperator);
+
+            // Assert
+            defaultFilterOperator
+                .Should()
+                .NotBeNull();
+            defaultFilterOperator
+                .IsDefault
+                .Should()
+                .BeTrue();
+        }
+
+        [Fact]
+        public void Provider_ReturnsFilterOperatorList()
+        {
+            // Arrange
+            IFilterOperatorProvider provider = new FilterOperatorProvider();
+
+            // Act
+            var filterOperators = provider.Operators;
+
+            // Assert
+            filterOperators
+                .Should()
+                .NotBeEmpty();
+        }
+    }
+}
