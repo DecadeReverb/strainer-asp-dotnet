8305909620e23e3ce783a371d483c07b95d4202e
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-06-08 21:56:20 +0200

    Add comments to filter term

--- src/Strainer/Models/Filtering/Terms/FilterTerm.cs
+++ src/Strainer/Models/Filtering/Terms/FilterTerm.cs
@@ -5,28 +5,69 @@ using System.Linq;
 
 namespace Fluorite.Strainer.Models.Filtering.Terms
 {
+    /// <summary>
+    /// Provides detailed information about filter expression.
+    /// </summary>
     public class FilterTerm : IFilterTerm, IEquatable<FilterTerm>
     {
-        public FilterTerm()
+        /// <summary>
+        /// Initializes new instance of <see cref="FilterTerm"/> class.
+        /// </summary>
+        public FilterTerm(string input)
         {
+            Input = input;
             Names = new List<string>();
             Values = new List<string>();
         }
 
-        public string Input { get; set; }
+        /// <summary>
+        /// Gets the original input, based on which current filter term was created.
+        /// </summary>
+        public string Input { get; }
 
+        /// <summary>
+        /// Gets or sets the list of names.
+        /// </summary>
         public IList<string> Names { get; set; }
 
+        /// <summary>
+        /// Gets or sets the unparsed <see cref="string"/> filter operator.
+        /// </summary>
         public string Operator { get; set; }
 
+        /// <summary>
+        /// Gets or sets a <see cref="bool"/> value indictating whether used
+        /// operator is case insensitive.
+        /// </summary>
         public bool OperatorIsCaseInsensitive { get; set; }
 
+        /// <summary>
+        /// Gets or sets a <see cref="bool"/> value indictating whether used
+        /// operator is a negated version of a different operator.
+        /// </summary>
         public bool OperatorIsNegated { get; set; }
 
+        /// <summary>
+        /// Gets or sets the parsed filter operator.
+        /// </summary>
         public IFilterOperator OperatorParsed { get; set; }
 
+        /// <summary>
+        /// Gets or sets the list of values.
+        /// </summary>
         public IList<string> Values { get; set; }
 
+        /// <summary>
+        /// Checks if current instance of <see cref="FilterTerm"/> is equal
+        /// to other <see cref="FilterTerm"/> instance.
+        /// </summary>
+        /// <param name="other">
+        /// Other <see cref="FilterTerm"/> instance.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if provided other <see cref="object"/>
+        /// instance is equal to the current one; otherwise <see langword="false"/>.
+        /// </returns>
         public bool Equals(FilterTerm other)
         {
             return other != null
--- src/Strainer/Models/Filtering/Terms/IFilterTerm.cs
+++ src/Strainer/Models/Filtering/Terms/IFilterTerm.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 
 namespace Fluorite.Strainer.Models.Filtering.Terms
 {
+    /// <summary>
+    /// Provides detailed information about filter expression.
+    /// </summary>
     public interface IFilterTerm
     {
+        /// <summary>
+        /// Gets the original input, based on which current filter term was created.
+        /// </summary>
         string Input { get; }
+
+        /// <summary>
+        /// Gets the list of names.
+        /// </summary>
         IList<string> Names { get; }
+
+        /// <summary>
+        /// Gets the unparsed <see cref="string"/> filter operator.
+        /// </summary>
         string Operator { get; }
+
+        /// <summary>
+        /// Gets a <see cref="bool"/> value indictating whether used
+        /// operator is case insensitive.
+        /// </summary>
         bool OperatorIsCaseInsensitive { get; }
+
+        /// <summary>
+        /// Gets a <see cref="bool"/> value indictating whether used
+        /// operator is a negated version of a different operator.
+        /// </summary>
         bool OperatorIsNegated { get; }
+
+        /// <summary>
+        /// Gets the parsed filter operator.
+        /// </summary>
         IFilterOperator OperatorParsed { get; }
+
+        /// <summary>
+        /// Gets the list of values.
+        /// </summary>
         IList<string> Values { get; }
     }
 }
