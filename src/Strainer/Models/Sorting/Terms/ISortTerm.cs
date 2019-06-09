12bb92b9597f9c3f8cf04a803eeaf4ec87bf48b0
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-06-09 16:12:21 +0200

    Add comments for sort term

--- src/Strainer/Models/Sorting/Terms/ISortTerm.cs
+++ src/Strainer/Models/Sorting/Terms/ISortTerm.cs
@@ -1,9 +1,24 @@
 ﻿namespace Fluorite.Strainer.Models.Sorting.Terms
 {
-	public interface ISortTerm
+    /// <summary>
+    /// Provides detailed information about sorting expression.
+    /// </summary>
+    public interface ISortTerm
     {
+        /// <summary>
+        /// Gets the original input, based on which current sort term was created.
+        /// </summary>
         string Input { get; }
+
+        /// <summary>
+        /// Gets a <see cref="bool"/> value indictating whether current sorting
+        /// direction is descending.
+        /// </summary>
         bool IsDescending { get; }
+
+        /// <summary>
+        /// Gets the name of sorting method.
+        /// </summary>
         string Name { get; }
     }
 }
--- src/Strainer/Models/Sorting/Terms/SortTerm.cs
+++ src/Strainer/Models/Sorting/Terms/SortTerm.cs
@@ -2,19 +2,46 @@
 
 namespace Fluorite.Strainer.Models.Sorting.Terms
 {
+    /// <summary>
+    /// Provides detailed information about sorting expression.
+    /// </summary>
     public class SortTerm : ISortTerm, IEquatable<SortTerm>
     {
+        /// <summary>
+        /// Initializes new instance of <see cref="SortTerm"/> class.
+        /// </summary>
         public SortTerm()
         {
 
         }
 
+        /// <summary>
+        /// Gets or sets the original input, based on which current sort term was created.
+        /// </summary>
         public string Input { get; set; }
 
+        /// <summary>
+        /// Gets or sets a <see cref="bool"/> value indictating whether current sorting
+        /// direction is descending.
+        /// </summary>
         public bool IsDescending { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of sorting method.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Checks if current instance of <see cref="SortTerm"/> is equal
+        /// to other <see cref="SortTerm"/> instance.
+        /// </summary>
+        /// <param name="other">
+        /// Other <see cref="SortTerm"/> instance.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if provided other <see cref="object"/>
+        /// instance is equal to the current one; otherwise <see langword="false"/>.
+        /// </returns>
         public bool Equals(SortTerm other)
         {
             return other != null
