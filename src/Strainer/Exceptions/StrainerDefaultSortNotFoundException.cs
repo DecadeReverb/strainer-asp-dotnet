91b65fd4be9df82700dcd38656f3c92988ff0651
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-07-14 15:04:38 +0200

    Add new strainer exception for default sort rule not found

---
+++ src/Strainer/Exceptions/StrainerDefaultSortNotFoundException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Fluorite.Strainer.Exceptions
+{
+    public class StrainerDefaultSortNotFoundException : StrainerException
+    {
+        public StrainerDefaultSortNotFoundException(Type entityType)
+        {
+            EntityType = entityType ?? throw new ArgumentNullException(nameof(entityType));
+        }
+
+        public StrainerDefaultSortNotFoundException(Type entityType, string message) : base(message)
+        {
+            EntityType = entityType ?? throw new ArgumentNullException(nameof(entityType));
+        }
+
+        public StrainerDefaultSortNotFoundException(Type entityType, string message, Exception innerException) : base(message, innerException)
+        {
+            EntityType = entityType ?? throw new ArgumentNullException(nameof(entityType));
+        }
+
+        protected StrainerDefaultSortNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+
+        public Type EntityType { get; }
+    }
+}
