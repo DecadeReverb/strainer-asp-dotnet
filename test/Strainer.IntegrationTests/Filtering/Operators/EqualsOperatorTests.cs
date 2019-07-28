0e6da84171243eed788c062f200e64be38dbaa4c
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-07-28 18:27:49 +0200

    Add integration tests for equals filter opertator

---
+++ test/Strainer.IntegrationTests/Filtering/Operators/EqualsOperatorTests.cs
@@ -0,0 +1,81 @@
+﻿using FluentAssertions;
+using Fluorite.Strainer.Models;
+using Fluorite.Strainer.TestModels;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Fluorite.Strainer.IntegrationTests.Filtering.Operators
+{
+    public class EqualsOperatorTests : StrainerFixtureBase
+    {
+        public EqualsOperatorTests(StrainerFactory factory) : base(factory)
+        {
+
+        }
+
+        [Fact]
+        public void Equals_Works_When_CaseSensivity_IsDisabled()
+        {
+            // Arrange
+            var source = new[]
+            {
+                new Comment
+                {
+                    Text = "foo",
+                },
+                new Comment
+                {
+                    Text = "bar",
+                },
+                new Comment
+                {
+                    Text = "FOO",
+                },
+            }.AsQueryable();
+            var processor = Factory.CreateDefaultProcessor(options => options.CaseSensitive = false);
+            var model = new StrainerModel
+            {
+                Filters = "Text==foo",
+            };
+
+            // Act
+            var result = processor.ApplyFiltering(model, source);
+
+            // Assert
+            result.Should().OnlyContain(b => b.Text.Equals("foo", StringComparison.OrdinalIgnoreCase));
+        }
+
+        [Fact]
+        public void Equals_Works_When_CaseSensivity_IsEnabled()
+        {
+            // Arrange
+            var source = new[]
+            {
+                new Comment
+                {
+                    Text = "foo",
+                },
+                new Comment
+                {
+                    Text = "bar",
+                },
+                new Comment
+                {
+                    Text = "FOO",
+                },
+            }.AsQueryable();
+            var processor = Factory.CreateDefaultProcessor(options => options.CaseSensitive = true);
+            var model = new StrainerModel
+            {
+                Filters = "Text==foo",
+            };
+
+            // Act
+            var result = processor.ApplyFiltering(model, source);
+
+            // Assert
+            result.Should().OnlyContain(b => b.Text.Equals("foo", StringComparison.Ordinal));
+        }
+    }
+}
