8a5f9b2f57bf2fc4ffbd6a1eec48e6c73f9968df
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-08-11 11:09:08 +0200

    Add tests for custom filter method mapper

---
+++ test/Strainer.UnitTests/Services/Filtering/CustomFilterMethodMapperTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Fluorite.Strainer.Models;
+using Fluorite.Strainer.Models.Filtering;
+using Fluorite.Strainer.Services.Filtering;
+using Fluorite.Strainer.TestModels;
+using System.Linq;
+using Xunit;
+
+namespace Fluorite.Strainer.UnitTests.Services.Filtering
+{
+    public class CustomFilterMethodMapperTests
+    {
+        [Fact]
+        public void Mapper_Adds_NewCustomMethod()
+        {
+            // Arrange
+            var options = new StrainerOptions();
+            var customFilterMethod = new CustomFilterMethod<Comment>
+            {
+                Function = context => context.Source.Where(c => c.DateCreated.Year > 2000),
+                Name = "XXI-century-comments",
+            };
+            var mapper = new CustomFilterMethodMapper(options);
+
+            // Act
+            mapper.AddMap(customFilterMethod);
+            var result = mapper.GetMethod<Comment>(customFilterMethod.Name);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().Be(customFilterMethod);
+        }
+    }
+}
