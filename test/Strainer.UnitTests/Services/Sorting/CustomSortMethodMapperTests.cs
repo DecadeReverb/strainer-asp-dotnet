d986fc474962047ad02a9b17a697eda49acf8f03
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-08-11 12:21:22 +0200

    Add tests for custom sort method mapper

---
+++ test/Strainer.UnitTests/Services/Sorting/CustomSortMethodMapperTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Fluorite.Strainer.Models;
+using Fluorite.Strainer.Models.Sorting;
+using Fluorite.Strainer.Services.Sorting;
+using Fluorite.Strainer.TestModels;
+using System.Linq;
+using Xunit;
+
+namespace Fluorite.Strainer.UnitTests.Services.Sorting
+{
+    public class CustomSortMethodMapperTests
+    {
+        [Fact]
+        public void Mapper_Adds_NewCustomMethod()
+        {
+            // Arrange
+            var options = new StrainerOptions();
+            var customSortMethod = new CustomSortMethod<Comment>
+            {
+                Function = context => context.Source
+                    .OrderBy(c => c.DateCreated)
+                    .ThenBy(c => c.Text),
+                Name = "DateCreatedThenText",
+            };
+            var mapper = new CustomSortMethodMapper(options);
+
+            // Act
+            mapper.AddMap(customSortMethod);
+            var result = mapper.GetMethod<Comment>(customSortMethod.Name);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().Be(customSortMethod);
+        }
+    }
+}
