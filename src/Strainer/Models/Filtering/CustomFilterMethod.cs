2579de4e9667566d5004c26089665c2b4f14447a
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-10-04 19:05:01 +0200

    Improve xml doc for filter term and custom filter method

--- src/Strainer/Models/Filtering/CustomFilterMethod.cs
+++ src/Strainer/Models/Filtering/CustomFilterMethod.cs
@@ -7,7 +7,7 @@ namespace Fluorite.Strainer.Models.Filtering
     /// Represents custom filter method.
     /// </summary>
     /// <typeparam name="TEntity">
-    /// The type of entity processed by custom method.
+    /// The type of entity processed by the custom method.
     /// </typeparam>
     public class CustomFilterMethod<TEntity> : ICustomFilterMethod<TEntity>
     {
@@ -21,12 +21,12 @@ namespace Fluorite.Strainer.Models.Filtering
         }
 
         /// <summary>
-        /// Gets the function used for custom filtering.
+        /// Gets or sets the function used for custom filtering.
         /// </summary>
         public Func<ICustomFilterMethodContext<TEntity>, IQueryable<TEntity>> Function { get; set; }
 
         /// <summary>
-        /// Gets the custom method name.
+        /// Gets or sets the custom method name.
         /// </summary>
         public string Name { get; set; }
     }
