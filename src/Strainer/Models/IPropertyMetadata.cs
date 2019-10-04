38c4073a94c5be83b2d5b761fd98c387e00667ee
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-10-04 18:48:21 +0200

    Add xml doc to strainer property attribute

--- src/Strainer/Models/IPropertyMetadata.cs
+++ src/Strainer/Models/IPropertyMetadata.cs
@@ -12,24 +12,33 @@ namespace Fluorite.Strainer.Models
         /// </summary>
         string DisplayName { get; }
 
+        /// <summary>
+        /// Gets a <see cref="bool"/> value indicating whether related
+        /// property should be used as a default (fallback) property when
+        /// sorting objects having related property.
+        /// </summary>
         bool IsDefaultSorting { get; }
 
+        /// <summary>
+        /// Gets a <see cref="bool"/> value indicating whether default
+        /// sorting should be performed in a descending way.
+        /// </summary>
         bool IsDefaultSortingDescending { get; }
 
         /// <summary>
         /// Gets a <see cref="bool"/> value indicating whether related
-        /// property is marked as filterable for Strainer.
+        /// property is marked as filterable.
         /// </summary>
         bool IsFilterable { get; }
 
         /// <summary>
         /// Gets a <see cref="bool"/> value indicating whether related
-        /// property is marked as sortable for Strainer.
+        /// property is marked as sortable.
         /// </summary>
         bool IsSortable { get; }
 
         /// <summary>
-        /// Gets the name under which property was marked.
+        /// Gets the name of related property.
         /// </summary>
         string Name { get; }
 
