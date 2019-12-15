834eb694e03b0f8864851875e09f1b1396c060a9
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2019-12-15 13:30:53 +0100

    Add integration tests for attribute metadata provider

---
+++ test/Strainer.IntegrationTests/Extensions/DependencyInjection/AttributeMetadataProviderTests.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using Fluorite.Extensions.DependencyInjection;
+using Fluorite.Strainer.Attributes;
+using Fluorite.Strainer.IntegrationTests.Services;
+using Fluorite.Strainer.Services.Metadata;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Xunit;
+
+namespace Fluorite.Strainer.IntegrationTests.Extensions.DependencyInjection
+{
+    public class AttributeMetadataProviderTests : StrainerFixtureBase
+    {
+        public AttributeMetadataProviderTests(StrainerFactory factory) : base(factory)
+        {
+
+        }
+
+        [Fact]
+        public void AttributeMetadataProvider_Works_For_Object_Attribute()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            services.AddStrainer<ApplicationStrainerProcessor>();
+            services.AddScoped<AttributeMetadataProvider>();
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Act
+            var attributeMetadataProvider = serviceProvider.GetRequiredService<AttributeMetadataProvider>();
+            var metadatas = attributeMetadataProvider.GetPropertyMetadatas<ObjectAttributeTestClass>();
+
+            // Assert
+            metadatas.Should().NotBeNullOrEmpty();
+            metadatas.Should().HaveCount(1);
+            metadatas.First().Name.Should().Be(nameof(ObjectAttributeTestClass.Id));
+        }
+
+        [Fact]
+        public void AttributeMetadataProvider_Works_For_Property_Attribute()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            services.AddStrainer<ApplicationStrainerProcessor>();
+            services.AddScoped<AttributeMetadataProvider>();
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Act
+            var attributeMetadataProvider = serviceProvider.GetRequiredService<AttributeMetadataProvider>();
+            var metadatas = attributeMetadataProvider.GetPropertyMetadatas<PropertyAttributeTestClass>();
+
+            // Assert
+            metadatas.Should().NotBeNullOrEmpty();
+            metadatas.Should().HaveCount(1);
+            metadatas.First().Should().BeAssignableTo<StrainerPropertyAttribute>();
+            metadatas.First().Name.Should().Be(nameof(PropertyAttributeTestClass.Id));
+        }
+
+        [StrainerObject(nameof(Id))]
+        private class ObjectAttributeTestClass
+        {
+            public int Id { get; set; }
+        }
+
+        private class PropertyAttributeTestClass
+        {
+            [StrainerProperty(IsDefaultSorting = true)]
+            public int Id { get; set; }
+        }
+    }
+}
