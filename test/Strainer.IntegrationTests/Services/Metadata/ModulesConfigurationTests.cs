b39c39a63da94a4554bf9136048a3df55b91d2c1
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2020-01-06 17:10:48 +0100

    Add more tests for module configuration

--- test/Strainer.IntegrationTests/Services/Metadata/ModulesConfigurationTests.cs
+++ test/Strainer.IntegrationTests/Services/Metadata/ModulesConfigurationTests.cs
@@ -1,10 +1,16 @@
 ﻿using FluentAssertions;
 using Fluorite.Extensions.DependencyInjection;
+using Fluorite.Strainer.Models.Filtering;
+using Fluorite.Strainer.Models.Filtering.Operators;
+using Fluorite.Strainer.Models.Sorting;
+using Fluorite.Strainer.Services.Filtering;
 using Fluorite.Strainer.Services.Metadata;
 using Fluorite.Strainer.Services.Modules;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using Xunit;
 
 namespace Fluorite.Strainer.IntegrationTests.Services.Metadata
@@ -32,7 +38,97 @@ namespace Fluorite.Strainer.IntegrationTests.Services.Metadata
             metadatas.First().Name.Should().Be(nameof(Post.Id));
         }
 
+        [Fact]
+        public void ModuleConfiguration_Works_For_Adding_Object()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            services.AddStrainer(new[] { typeof(PropertyTestModule) });
+            var serviceProvider = services.BuildServiceProvider();
+            var metadataProviders = serviceProvider.GetRequiredService<IEnumerable<IMetadataProvider>>();
+            var fluentApiMetadataProvider = metadataProviders
+                .OfType<FluentApiMetadataProvider>()
+                .Single();
+
+            // Act
+            var metadata = fluentApiMetadataProvider.GetDefaultMetadata<Comment>();
+
+            // Assert
+            metadata.Should().NotBeNull();
+            metadata.Name.Should().Be(nameof(Comment.Id));
+        }
+
+        [Fact]
+        public void ModuleConfiguration_Works_For_Adding_Filter_Operator()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            services.AddStrainer(new[] { typeof(PropertyTestModule) });
+            var operatorSymbol = "###";
+            var serviceProvider = services.BuildServiceProvider();
+            var filterOperators = serviceProvider.GetRequiredService<IReadOnlyDictionary<string, IFilterOperator>>();
+
+            // Act
+            var result = filterOperators.TryGetValue(operatorSymbol, out var filterOperator);
+
+            // Assert
+            result.Should().BeTrue();
+            filterOperator.Should().NotBeNull();
+            filterOperator.Symbol.Should().Be(operatorSymbol);
+        }
+
+        [Fact]
+        public void ModuleConfiguration_Works_For_Adding_Custom_Filter_Method()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            services.AddStrainer(new[] { typeof(PropertyTestModule) });
+            var customMethodName = "TestCustomFilterMethod";
+            var serviceProvider = services.BuildServiceProvider();
+            var customFilterMethods = serviceProvider
+                .GetRequiredService<IReadOnlyDictionary<Type, IReadOnlyDictionary<string, ICustomFilterMethod>>>();
+
+            // Act
+            var result = customFilterMethods.TryGetValue(typeof(Post), out var customFilterMethodsForPost);
+            var result2 = customFilterMethodsForPost.TryGetValue(customMethodName, out var customFilterMethod);
+
+            // Assert
+            result.Should().BeTrue();
+            result2.Should().BeTrue();
+            customFilterMethodsForPost.Should().NotBeNullOrEmpty();
+            customFilterMethod.Should().NotBeNull();
+            customFilterMethod.Name.Should().Be(customMethodName);
+        }
+
+        [Fact]
+        public void ModuleConfiguration_Works_For_Adding_Custom_Sort_Method()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            services.AddStrainer(new[] { typeof(PropertyTestModule) });
+            var customMethodName = "TestCustomSortMethod";
+            var serviceProvider = services.BuildServiceProvider();
+            var customSortMethods = serviceProvider
+                .GetRequiredService<IReadOnlyDictionary<Type, IReadOnlyDictionary<string, ICustomSortMethod>>>();
+
+            // Act
+            var result = customSortMethods.TryGetValue(typeof(Post), out var customSortMethodsForPost);
+            var result2 = customSortMethodsForPost.TryGetValue(customMethodName, out var customSortMethod);
+
+            // Assert
+            result.Should().BeTrue();
+            result2.Should().BeTrue();
+            customSortMethodsForPost.Should().NotBeNullOrEmpty();
+            customSortMethod.Should().NotBeNull();
+            customSortMethod.Name.Should().Be(customMethodName);
+        }
+
         private class Post
+        {
+            public int Id { get; set; }
+        }
+
+        private class Comment
         {
             public int Id { get; set; }
         }
@@ -45,6 +141,20 @@ namespace Fluorite.Strainer.IntegrationTests.Services.Metadata
                     .IsFilterable()
                     .IsSortable()
                     .IsDefaultSort();
+
+                AddObject<Comment>(comment => comment.Id)
+                    .IsFilterable()
+                    .IsSortable();
+
+                AddFilterOperator(symbol: "###")
+                    .HasName("hash")
+                    .HasExpression(context => Expression.Constant(true));
+
+                AddCustomFilterMethod<Post>("TestCustomFilterMethod")
+                    .WithFunction(context => context.Source.Where(post => post.Id == 1));
+
+                AddCustomSortMethod<Post>("TestCustomSortMethod")
+                    .WithFunction(context => context.Source.OrderBy(post => post.Id));
             }
         }
     }
