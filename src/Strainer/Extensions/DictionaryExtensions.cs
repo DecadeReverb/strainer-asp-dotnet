30b86e3fcebb09a7c52a3462ac15fe53318d350f
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2020-07-19 19:22:46 +0200

    Split LINQ extensions into seperate static classes

---
+++ src/Strainer/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Fluorite.Strainer.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static TDictionary MergeLeft<TDictionary, TKey, TValue>(
+            this TDictionary source,
+            params IDictionary<TKey, TValue>[] others)
+            where TDictionary : IDictionary<TKey, TValue>, new()
+        {
+            var resultDictionary = new TDictionary();
+
+            foreach (var dictionary in new List<IDictionary<TKey, TValue>> { source }.Concat(others))
+            {
+                foreach (var pair in dictionary)
+                {
+                    resultDictionary[pair.Key] = pair.Value;
+                }
+            }
+
+            return resultDictionary;
+        }
+
+        public static IReadOnlyDictionary<TKey, TValue> ToReadOnly<TKey, TValue>(
+            this IDictionary<TKey, TValue> source)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return new ReadOnlyDictionary<TKey, TValue>(source);
+        }
+    }
+}
---
+++ src/Strainer/Extensions/QuerableExtensions.cs
@@ -0,0 +1,69 @@
+﻿using Fluorite.Strainer.Models.Sorting;
+using System;
+using System.Linq;
+
+namespace Fluorite.Strainer.Extensions
+{
+    public static class QuerableExtensions
+    {
+        /// <summary>
+        /// Sorts the elements of a sequence in an order according to
+        /// <see cref="ISortExpression{TEntity}"/>.
+        /// </summary>
+        /// <typeparam name="TEntity">
+        /// The type of entity stored in a sequence.
+        /// </typeparam>
+        /// <param name="source">
+        /// The source sequence of elements.
+        /// </param>
+        /// <param name="sortExpression">
+        /// The sort expression providing information about sorting order and key.
+        /// </param>
+        /// <returns>
+        /// An instance of ordered queryable sequence.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="sortExpression"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source"/> is <see langword="null"/>.
+        /// </exception>
+        public static IOrderedQueryable<TEntity> OrderWithSortExpression<TEntity>(
+            this IQueryable<TEntity> source,
+            ISortExpression<TEntity> sortExpression)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (sortExpression == null)
+            {
+                throw new ArgumentNullException(nameof(sortExpression));
+            }
+
+            if (sortExpression.IsDescending)
+            {
+                if (sortExpression.IsSubsequent)
+                {
+                    return (source as IOrderedQueryable<TEntity>).ThenByDescending(sortExpression.Expression);
+                }
+                else
+                {
+                    return source.OrderByDescending(sortExpression.Expression);
+                }
+            }
+            else
+            {
+                if (sortExpression.IsSubsequent)
+                {
+                    return (source as IOrderedQueryable<TEntity>).ThenBy(sortExpression.Expression);
+                }
+                else
+                {
+                    return source.OrderBy(sortExpression.Expression);
+                }
+            }
+        }
+    }
+}
