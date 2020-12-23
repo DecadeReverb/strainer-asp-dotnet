3f2e4205678fe06c0316f543cd4376854f2b3a5d
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2020-12-23 17:46:12 +0100

    Move last tests from general to dedicated test classes

---
+++ test/Strainer.IntegrationTests/Sorting/BoolSortingTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using Fluorite.Strainer.Attributes;
+using Fluorite.Strainer.IntegrationTests.Fixtures;
+using Fluorite.Strainer.Models;
+using Fluorite.Strainer.Models.Sorting;
+using System.Linq;
+using Xunit;
+
+namespace Fluorite.Strainer.IntegrationTests.Sorting
+{
+    public class BoolSortingTests : StrainerFixtureBase
+    {
+        public BoolSortingTests(StrainerFactory factory) : base(factory)
+        {
+
+        }
+
+        [Fact]
+        public void IsSortableBools()
+        {
+            // Arrange
+            var source = new[]
+            {
+                new Post
+                {
+                    IsDraft = true,
+                },
+                new Post
+                {
+                    IsDraft = false,
+                },
+            }.AsQueryable();
+            var model = new StrainerModel()
+            {
+                Sorts = "-IsDraft"
+            };
+            var processor = Factory.CreateDefaultProcessor(opt =>
+            {
+                opt.DefaultSortingWay = SortingWay.Ascending;
+            });
+
+            // Act
+            var result = processor.ApplySorting(model, source);
+
+            // Assert
+            result.Should().BeInDescendingOrder(e => e.IsDraft);
+        }
+
+        private class Post
+        {
+            [StrainerProperty(IsDefaultSorting = true)]
+            public bool IsDraft { get; set; }
+        }
+    }
+}
