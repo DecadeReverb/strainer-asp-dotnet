349a8ed67a1ef8e18fd34cb5589d7946648e5db7
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2021-01-18 23:19:13 +0100

    Add generic strainer module and module builder

---
+++ src/Strainer/Services/Modules/IStrainerModule`1.cs
@@ -0,0 +1,47 @@
+﻿using Fluorite.Strainer.Models.Filtering;
+using Fluorite.Strainer.Models.Filtering.Operators;
+using Fluorite.Strainer.Models.Metadata;
+using Fluorite.Strainer.Models.Sorting;
+using System.Collections.Generic;
+
+namespace Fluorite.Strainer.Services.Modules
+{
+    /// <summary>
+    /// Stores Strainer configuration in an encapsulated form of a module.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of model for which this module is for.
+    /// </typeparam>
+    public interface IStrainerModule<T>
+    {
+        /// <summary>
+        /// Gets the object custom filter methods dictionary.
+        /// </summary>
+        IDictionary<string, ICustomFilterMethod> CustomFilterMethods { get; }
+
+        /// <summary>
+        /// Gets the object custom sorting methods dictionary.
+        /// </summary>
+        IDictionary<string, ICustomSortMethod> CustomSortMethods { get; }
+
+        /// <summary>
+        /// Gets the default metadata.
+        /// </summary>
+        IPropertyMetadata DefaultMetadata { get; }
+
+        /// <summary>
+        /// Gets the object filter operator dictionary.
+        /// </summary>
+        IDictionary<string, IFilterOperator> FilterOperators { get; }
+
+        /// <summary>
+        /// Gets the object metadata.
+        /// </summary>
+        IObjectMetadata ObjectMetadata { get; }
+
+        /// <summary>
+        /// Gets the object property dictionary.
+        /// </summary>
+        IDictionary<string, IPropertyMetadata> PropertyMetadata { get; }
+    }
+}
---
+++ src/Strainer/Services/Modules/StrainerModule`1.cs
@@ -0,0 +1,71 @@
+﻿using Fluorite.Strainer.Models.Filtering;
+using Fluorite.Strainer.Models.Filtering.Operators;
+using Fluorite.Strainer.Models.Metadata;
+using Fluorite.Strainer.Models.Sorting;
+using System.Collections.Generic;
+
+namespace Fluorite.Strainer.Services.Modules
+{
+    /// <summary>
+    /// Provides configuration information about metadata, filter operators
+    /// and custom methods for Strainer.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of model for which this module is for.
+    /// </typeparam>
+    public abstract class StrainerModule<T> : IStrainerModule<T>
+    {
+        /// <summary>
+        /// Initializes new instance of <see cref="StrainerModule"/> class.
+        /// </summary>
+        protected StrainerModule()
+        {
+            CustomFilterMethods = new Dictionary<string, ICustomFilterMethod>();
+            CustomSortMethods = new Dictionary<string, ICustomSortMethod>();
+            DefaultMetadata = null;
+            FilterOperators = new Dictionary<string, IFilterOperator>();
+            ObjectMetadata = null;
+            PropertyMetadata = new Dictionary<string, IPropertyMetadata>();
+        }
+
+        /// <summary>
+        /// Gets the object custom filter methods dictionary.
+        /// </summary>
+        public IDictionary<string, ICustomFilterMethod> CustomFilterMethods { get; }
+
+        /// <summary>
+        /// Gets the object custom sorting methods dictionary.
+        /// </summary>
+        public IDictionary<string, ICustomSortMethod> CustomSortMethods { get; }
+
+        /// <summary>
+        /// Gets the object default dictionary.
+        /// </summary>
+        public IPropertyMetadata DefaultMetadata { get; }
+
+        /// <summary>
+        /// Gets the object filter operator dictionary.
+        /// </summary>
+        public IDictionary<string, IFilterOperator> FilterOperators { get; }
+
+        /// <summary>
+        /// Gets the object metadata dictionary.
+        /// </summary>
+        public IObjectMetadata ObjectMetadata { get; }
+
+        /// <summary>
+        /// Gets the object property dictionary.
+        /// </summary>
+        public IDictionary<string, IPropertyMetadata> PropertyMetadata { get; }
+
+        /// <summary>
+        /// Loads configuration in module.
+        /// <para/>
+        /// Override this method to specify configuration for this module.
+        /// </summary>
+        /// <param name="builder">
+        /// The Strainer Module builder.
+        /// </param>
+        public abstract void Load(IStrainerModuleBuilder<T> builder);
+    }
+}
