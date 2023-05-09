0cebbba385b945c2d4131ce01e124d9d9efb0fc3
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2023-05-09 19:10:03 +0200

    Use standard string splitting for filter term values parsing

--- src/Strainer/Services/Filtering/FilterTermValuesParser.cs
+++ src/Strainer/Services/Filtering/FilterTermValuesParser.cs
@@ -1,11 +1,7 @@
-﻿using System.Text.RegularExpressions;
-
-namespace Fluorite.Strainer.Services.Filtering
+﻿namespace Fluorite.Strainer.Services.Filtering
 {
     public class FilterTermValuesParser : IFilterTermValuesParser
     {
-        private const string EscapedPipePattern = @"(?<!($|[^\\])(\\\\)*?\\)\|";
-
         public IList<string> Parse(string input)
         {
             if (string.IsNullOrEmpty(input))
@@ -13,8 +9,8 @@ namespace Fluorite.Strainer.Services.Filtering
                 return new List<string>();
             }
 
-            return Regex.Split(input, EscapedPipePattern)
-                .Select(t => t.Trim())
+            return input.Split(new[] { '|' }, StringSplitOptions.None)
+                .Select(t => t.Trim().Replace(@"\,", ","))
                 .ToList();
         }
     }
