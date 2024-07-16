b38c16d29b0cdd085c979f349a3ccc0869877bc5
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2024-07-16 23:14:54 +0200

    Add more unit tests for metadata mapper

--- test/Strainer.UnitTests/Services/Metadata/MetadataMapperTests.cs
+++ test/Strainer.UnitTests/Services/Metadata/MetadataMapperTests.cs
@@ -2,6 +2,7 @@
 using Fluorite.Strainer.Models.Metadata;
 using Fluorite.Strainer.Services;
 using Fluorite.Strainer.Services.Metadata;
+using System.Linq.Expressions;
 
 namespace Fluorite.Strainer.UnitTests.Services.Metadata;
 
@@ -11,11 +12,10 @@ public class MetadataMapperTests
     public void AddPropertyMetadata_Adds_PropertyMetadata_Via_AddPropertyMetadata()
     {
         // Arrange
-        var optionsMock = Substitute.For<IStrainerOptionsProvider>();
-        optionsMock
+        var optionsProvider = Substitute.For<IStrainerOptionsProvider>();
+        optionsProvider
             .GetStrainerOptions()
             .Returns(new StrainerOptions());
-        var optionsProvider = optionsMock;
         var propertyInfoProviderMock = Substitute.For<IPropertyInfoProvider>();
         var mapper = new MetadataMapper(optionsProvider, propertyInfoProviderMock);
         var metadata = new PropertyMetadata()
@@ -38,11 +38,10 @@ public class MetadataMapperTests
     public void AddPropertyMetadata_Adds_Different_PropertyMetadata_For_Already_Existing_PropertyMetadata()
     {
         // Arrange
-        var optionsMock = Substitute.For<IStrainerOptionsProvider>();
-        optionsMock
+        var optionsProvider = Substitute.For<IStrainerOptionsProvider>();
+        optionsProvider
             .GetStrainerOptions()
             .Returns(new StrainerOptions());
-        var optionsProvider = optionsMock;
         var propertyInfoProviderMock = Substitute.For<IPropertyInfoProvider>();
         var mapper = new MetadataMapper(optionsProvider, propertyInfoProviderMock);
         var firstMetadata = new PropertyMetadata()
@@ -74,11 +73,10 @@ public class MetadataMapperTests
     public void AddPropertyMetadata_Adds_AlreadyExistingPropertyMetadata_Via_AddPropertyMetadata_Without_Duplicating()
     {
         // Arrange
-        var optionsMock = Substitute.For<IStrainerOptionsProvider>();
-        optionsMock
+        var optionsProvider = Substitute.For<IStrainerOptionsProvider>();
+        optionsProvider
             .GetStrainerOptions()
             .Returns(new StrainerOptions());
-        var optionsProvider = optionsMock;
         var propertyInfoProviderMock = Substitute.For<IPropertyInfoProvider>();
         var mapper = new MetadataMapper(optionsProvider, propertyInfoProviderMock);
         var metadata = new PropertyMetadata()
@@ -102,11 +100,10 @@ public class MetadataMapperTests
     public void Property_Throws_Exception_With_FluentApiMetadataSourceType_Disabled()
     {
         // Arrange
-        var optionsMock = Substitute.For<IStrainerOptionsProvider>();
-        optionsMock
+        var optionsProvider = Substitute.For<IStrainerOptionsProvider>();
+        optionsProvider
             .GetStrainerOptions()
             .Returns(new StrainerOptions { MetadataSourceType = MetadataSourceType.Attributes });
-        var optionsProvider = optionsMock;
         var propertyInfoProviderMock = Substitute.For<IPropertyInfoProvider>();
         var mapper = new MetadataMapper(optionsProvider, propertyInfoProviderMock);
 
@@ -118,21 +115,93 @@ public class MetadataMapperTests
     public void AddPropertyMetadata_Throws_Exception_With_FluentApiMetadataSourceType_Disabled()
     {
         // Arrange
-        var optionsMock = Substitute.For<IStrainerOptionsProvider>();
-        optionsMock
+        var optionsProvider = Substitute.For<IStrainerOptionsProvider>();
+        optionsProvider
             .GetStrainerOptions()
             .Returns(new StrainerOptions { MetadataSourceType = MetadataSourceType.Attributes });
-        var optionsProvider = optionsMock;
         var propertyInfoProviderMock = Substitute.For<IPropertyInfoProvider>();
         var mapper = new MetadataMapper(optionsProvider, propertyInfoProviderMock);
-        var metadata = new PropertyMetadata()
-        {
-            Name = nameof(Post.Id),
-            PropertyInfo = typeof(Post).GetProperty(nameof(Post.Id)),
-        };
+        var metadata = Substitute.For<IPropertyMetadata>();
 
         // Act & Assert
         Assert.Throws<InvalidOperationException>(() => mapper.AddPropertyMetadata<Post>(metadata));
+    }
+
+    [Fact]
+    public void AddObjectMetadata_Adds_ObjectMetadata()
+    {
+        // Arrange
+        var modelType = typeof(Post);
+        var optionsProvider = Substitute.For<IStrainerOptionsProvider>();
+        optionsProvider
+            .GetStrainerOptions()
+            .Returns(new StrainerOptions());
+        var propertyInfoProviderMock = Substitute.For<IPropertyInfoProvider>();
+        var mapper = new MetadataMapper(optionsProvider, propertyInfoProviderMock);
+        var objectMetadata = Substitute.For<IObjectMetadata>();
+
+        // Act
+        mapper.AddObjectMetadata<Post>(objectMetadata);
+
+        // Assert
+        mapper.ObjectMetadata.Should().NotBeEmpty();
+        mapper.ObjectMetadata.Keys.Should().BeEquivalentTo(new[] { modelType });
+        mapper.ObjectMetadata[modelType].Should().BeSameAs(objectMetadata);
+    }
+
+    [Fact]
+    public void AddObjectMetadata_Throws_Exception_With_FluentApiMetadataSourceType_Disabled()
+    {
+        // Arrange
+        var optionsProvider = Substitute.For<IStrainerOptionsProvider>();
+        optionsProvider
+            .GetStrainerOptions()
+            .Returns(new StrainerOptions { MetadataSourceType = MetadataSourceType.Attributes });
+        var propertyInfoProviderMock = Substitute.For<IPropertyInfoProvider>();
+        var mapper = new MetadataMapper(optionsProvider, propertyInfoProviderMock);
+        var objectMetadata = Substitute.For<IObjectMetadata>();
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => mapper.AddObjectMetadata<Post>(objectMetadata));
+    }
+
+    [Fact]
+    public void Object_AddObjectMetadata_Adds_ObjectMetadata_And_ReturnsBuilder()
+    {
+        // Arrange
+        var modelType = typeof(Post);
+        var optionsProvider = Substitute.For<IStrainerOptionsProvider>();
+        optionsProvider
+            .GetStrainerOptions()
+            .Returns(new StrainerOptions());
+        var propertyInfoProviderMock = Substitute.For<IPropertyInfoProvider>();
+        var mapper = new MetadataMapper(optionsProvider, propertyInfoProviderMock);
+        Expression<Func<Post, object>> expression = x => x.Id;
+
+        // Act
+        var builder = mapper.Object(expression);
+
+        // Assert
+        builder.Should().NotBeNull();
+
+        mapper.ObjectMetadata.Should().NotBeEmpty();
+        mapper.ObjectMetadata.Keys.Should().BeEquivalentTo(new[] { modelType });
+        mapper.ObjectMetadata[modelType].Should().NotBeNull();
+    }
+
+    [Fact]
+    public void Object_Throws_Exception_With_FluentApiMetadataSourceType_Disabled()
+    {
+        // Arrange
+        var optionsProvider = Substitute.For<IStrainerOptionsProvider>();
+        optionsProvider
+            .GetStrainerOptions()
+            .Returns(new StrainerOptions { MetadataSourceType = MetadataSourceType.Attributes });
+        var propertyInfoProviderMock = Substitute.For<IPropertyInfoProvider>();
+        var mapper = new MetadataMapper(optionsProvider, propertyInfoProviderMock);
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => mapper.Object<Post>(x => x.Id));
     }
 
     private class Post
