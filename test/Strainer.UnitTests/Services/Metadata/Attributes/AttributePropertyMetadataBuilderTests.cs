c2cc6578e2031a5fe47cfeabda9a6b04b615284a
Wilton <20190555+DecadeReverb@users.noreply.github.com>
2024-09-05 01:08:05 +0200

    Add unit tests for attribute property metadata builder

---
+++ test/Strainer.UnitTests/Services/Metadata/Attributes/AttributePropertyMetadataBuilderTests.cs
@@ -0,0 +1,39 @@
+﻿using Fluorite.Strainer.Attributes;
+using Fluorite.Strainer.Services.Metadata.Attributes;
+using System.Reflection;
+
+namespace Fluorite.Strainer.UnitTests.Services.Metadata.Attributes;
+
+public class AttributePropertyMetadataBuilderTests
+{
+    private readonly AttributePropertyMetadataBuilder _builder;
+
+    public AttributePropertyMetadataBuilderTests()
+    {
+        _builder = new ();
+    }
+
+    [Fact]
+    public void Should_Return_DefaultPropertyMetadata()
+    {
+        // Arrange
+        var attribute = new StrainerObjectAttribute("Id");
+        var propertyName = "foo";
+        var propertyInfo = Substitute.For<PropertyInfo>();
+
+        propertyInfo.Name.Returns(propertyName);
+
+        // Act
+        var result = _builder.BuildDefaultPropertyMetadata(attribute, propertyInfo);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.DisplayName.Should().BeNull();
+        result.IsDefaultSorting.Should().BeTrue();
+        result.IsDefaultSortingDescending.Should().Be(attribute.IsDefaultSortingDescending);
+        result.IsFilterable.Should().Be(attribute.IsFilterable);
+        result.IsSortable.Should().Be(attribute.IsSortable);
+        result.Name.Should().Be(propertyName);
+        result.PropertyInfo.Should().BeSameAs(propertyInfo);
+    }
+}
